28760c6a5985f37a4be93e83769ba1e4266e0be1
ecomcyrixian <ecomcyrixian@gmail.com>
2025-09-29 21:35:06 +0800

    Pricing section has been added

--- front-page.php
+++ front-page.php
@@ -9,5 +9,6 @@
 <?php get_template_part('partials/home/grid-layout/index') ?>
 <?php get_template_part('partials/home/banner/index') ?>
 <?php get_template_part('partials/home/key-features/index') ?>
+<?php get_template_part('partials/home/pricing/index') ?>
 
 <?php get_footer(); ?>
