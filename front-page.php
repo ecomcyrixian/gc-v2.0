7f895567706054d7ae7319ca01609463e7379439
ecomcyrixian <ecomcyrixian@gmail.com>
2025-10-01 15:00:47 +0800

    Hiring, Key features, Banners and FAQ have been added

--- front-page.php
+++ front-page.php
@@ -7,8 +7,11 @@
 <?php get_header('home'); ?>
 
 <?php get_template_part('partials/home/grid-layout/index') ?>
-<?php get_template_part('partials/home/banner/index') ?>
+<?php get_template_part('partials/home/banner/index-1') ?>
 <?php get_template_part('partials/home/key-features/index') ?>
 <?php get_template_part('partials/home/pricing/index') ?>
+<?php get_template_part('partials/home/hiring/index') ?>
+<?php get_template_part('partials/faq/faq-home') ?>
+<?php get_template_part('partials/home/banner/index-2') ?>
 
 <?php get_footer(); ?>
