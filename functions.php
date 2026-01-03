f4cd75001f48d8b878fdb1f731d5ecada2acbd8e
MonicaGCheck <monica@gcheck.com>
2026-01-03 06:23:57 +0000

    fix hover issue

--- functions.php
+++ functions.php
@@ -149,6 +149,54 @@ class Walker_Nav_Menu_With_Description extends Walker_Nav_Menu {
  */
 class Desktop_Mega_Walker extends Walker_Nav_Menu_With_Description {
 
+    public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+        $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+
+        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+        $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+
+        $class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+        $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+        $id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+
+        $output .= $indent . '<li' . $id . $class_names .'>';
+
+        $atts = array();
+        $atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
+        $atts['target'] = ! empty( $item->target )     ? $item->target     : '';
+        $atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
+        $atts['href']   = ! empty( $item->url )        ? $item->url        : '';
+        
+        if ( $depth === 0 ) {
+            $atts['class'] = 'mega-menu-trigger';
+        }
+
+        $atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args );
+
+        $attributes = '';
+        foreach ( $atts as $attr => $value ) {
+            if ( ! empty( $value ) ) {
+                $value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+                $attributes .= ' ' . $attr . '="' . $value . '"';
+            }
+        }
+
+        $description = get_field( 'menu_item_description', $item->ID );
+
+        $title = apply_filters( 'the_title', $item->title, $item->ID );
+
+        $item_output  = $args->before;
+        $item_output .= '<a'. $attributes .'>';
+        $item_output .= $args->link_before . $title . $args->link_after;
+        if ( $description ) {
+            $item_output .= '<span class="menu-item-description">' . $description . '</span>';
+        }
+        $item_output .= '</a>';
+        $item_output .= $args->after;
+
+        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+    }
+
     public function start_lvl( &$output, $depth = 0, $args = array() ) {
         if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
             $t = '';
